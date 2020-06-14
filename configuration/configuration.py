43cfcb4cdc6dc199240be71cee7818b24c08c20d
Yury Kuptsou <ykuptsou@iotecha.com>
2020-06-14 14:08:59 +0300

    Adding configuration
    Passing configurable variables from environment, throwing NameError in case any of required keys is missing

---
+++ configuration/configuration.py
@@ -0,0 +1,39 @@
+import os
+
+
+class Configuration(object):
+    """
+    Main configuration object.
+    Requires API_ACCESS_KEY, TM4J_API_URL, PROJECT_KEY to be defined in environment.
+    """
+    def __init__(self):
+        self._check_required_keys_exist()
+        self.api_access_key = os.environ["API_ACCESS_KEY"]
+        self.tm4j_api_url = os.environ["TM4J_API_URL"]
+        self.project_key = os.environ["PROJECT_KEY"]
+
+    @staticmethod
+    def _check_required_keys_exist():
+        """
+        Checks if required API_ACCESS_KEY, TM4J_API_URL, PROJECT_KEY exist in environment variables.
+
+        :raise NameError: raised if any of required keys is missing
+
+        :return: None
+        :rtype: None
+        """
+        missing_keys = []
+        for key in ["API_ACCESS_KEY", "TM4J_API_URL", "PROJECT_KEY"]:
+            try:
+                os.environ[key]
+            except KeyError as e:
+                missing_keys.append(e.args[0])
+        if missing_keys:
+            raise NameError(
+                f"Environment variables missing: {' ,'.join(missing_keys)}"
+            )
+
+        return None
+
+
+CONFIG = Configuration()
--- tm4j_api/tm4j_api.py
+++ tm4j_api/tm4j_api.py
@@ -1,15 +1,11 @@
 import requests
 
-# TODO: exporting TM4L API URL from config file
-TM4J_API_URL = "https://api.adaptavist.io/tm4j/v2"
+from configuration.configuration import CONFIG
 
-# TODO: need to understand how to configure project key - static or dynamic
-PROJECT_KEY = ""
 
-# TODO: exporting API_ACCESS_KEY from config file
-AUTH_HEADER = {"Authorization": f"Bearer {API_ACCESS_KEY}"}
-
-TEST_CYCLE_KEY = None
+TM4J_API_URL = CONFIG.tm4j_api_url
+PROJECT_KEY = CONFIG.project_key
+AUTH_HEADER = {"Authorization": f"Bearer {CONFIG.api_access_key}"}
 
 
 def create_test_cycle(test_cycle_name: str, **kwargs) -> str:
@@ -32,8 +28,6 @@ def create_test_cycle(test_cycle_name: str, **kwargs) -> str:
     :return: TM4J test cycle key
     :rtype str
     """
-    global TEST_CYCLE_KEY
-
     payload = {"projectKey": PROJECT_KEY, "name": test_cycle_name}
 
     # TODO: key/type checks?
@@ -45,15 +39,17 @@ def create_test_cycle(test_cycle_name: str, **kwargs) -> str:
         url=f"{TM4J_API_URL}/testcycles", json=payload, headers=AUTH_HEADER
     )
 
-    TEST_CYCLE_KEY = response.json()["key"]
-    return TEST_CYCLE_KEY
+    return response.json()["key"]
 
 
 def create_test_execution_result(
-    test_case_key: str, execution_status: str, **kwargs
+    test_cycle_key: str, test_case_key: str, execution_status: str, **kwargs
 ) -> None:
     """
     Creates test result for particular test case in test run
+
+    :param test_cycle_key: Key of TM4J test cycle to put test execution to
+    :type test_cycle_key: str
 
     :param test_case_key: Key of test case the execution applies to
     :type test_case_key: str
@@ -66,7 +62,6 @@ def create_test_execution_result(
         :keyword testScriptResults (List[Dict[str, str]]): List of objects with test steps results:
                     statusName (str), actualEndDate (str, yyyy-MM-dd'T'HH:mm:ss'Z'), actualResult (str).
         :keyword actualEndDate (str): Date test was executed. Format: yyyy-MM-dd'T'HH:mm:ss'Z'
-        :keyword actualResult (str): free text field to provide more info on result of step execution.
         :keyword environmentName (str): Environment assigned to the test case.
         :keyword executionTime (int): Actual execution time in milliseconds.
         :keyword executedById (str): Atlassian Account ID of the user who executes the test.
@@ -78,7 +73,7 @@ def create_test_execution_result(
     """
     payload = {
         "projectKey": PROJECT_KEY,
-        "testCycleKey": TEST_CYCLE_KEY,
+        "testCycleKey": test_cycle_key,
         "testCaseKey": test_case_key,
         "statusName": execution_status,
     }
