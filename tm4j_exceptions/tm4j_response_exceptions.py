3f704d671e3d60cf4b13d1acf7bd3bca503f0139
Yury Kuptsou <ykuptsou@iotecha.com>
2020-06-21 16:02:08 +0300

    Adding exceptions processing
    Added processing for TM4J API response. All keyword arguments moved from kwargs dict to explicit params with typehints.

---
+++ tm4j_exceptions/tm4j_response_exceptions.py
@@ -0,0 +1,38 @@
+import requests
+
+
+class TM4JResponseException(Exception):
+    """
+    Class to represent unsuccessful TM4J API operations
+    """
+
+    def __init__(self, message):
+        self.message = message
+
+
+def check_tm4j_api_response(
+    response: requests.models.Response, expected_status_code: int = 201
+) -> None:
+    """
+    Checking response from TM4J API
+
+    :param response: requests response object
+    :type response: requests.models.Response
+
+    :param expected_status_code: expected response status code, 201 by default
+    :type expected_status_code: int
+
+    :raise TM4JResponseException: raised if actual response status code doesn't match to expected
+
+    :return: None
+    :rtype: None
+    """
+    try:
+        assert response.status_code == expected_status_code
+    except AssertionError:
+        raise TM4JResponseException(
+            f"Response status code: {response.status_code},\n"
+            f"response message: {response.text}"
+        )
+    finally:
+        return None
